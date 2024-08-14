94cb5fcee509e7cf93b06b559bd1a6f5bf886e31
천영상 <cys07028@naver.com>
2024-08-15 00:35:15 +0900

    [Solve] A x B

---
+++ boj/java/step/b_01_input_output_and_arithmetic_operations/p_10998.java
@@ -0,0 +1,15 @@
+package java.step.b_01_input_output_and_arithmetic_operations;
+
+import java.util.Scanner;
+
+public class p_10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+
+        sc.close();
+    }
+}
