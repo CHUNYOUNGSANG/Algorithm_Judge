17ea022061127d227fa0114abc71953c0acace83
천영상 <cys07028@naver.com>
2024-08-15 00:38:38 +0900

    [Solve] 사칙연산

---
+++ boj/java/step/b_01_input_output_and_arithmetic_operations/p_10869.java
@@ -0,0 +1,19 @@
+package java.step.b_01_input_output_and_arithmetic_operations;
+
+import java.util.Scanner;
+
+public class p_10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+
+        sc.close();
+    }
+}
