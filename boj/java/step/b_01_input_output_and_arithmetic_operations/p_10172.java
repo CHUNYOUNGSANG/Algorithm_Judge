470bef9dac9bbdc9ff800a6289bd56c91e16128a
천영상 <cys07028@naver.com>
2024-08-15 00:47:53 +0900

    [Solve] 개

---
+++ boj/java/step/b_01_input_output_and_arithmetic_operations/p_10172.java
@@ -0,0 +1,11 @@
+package java.step.b_01_input_output_and_arithmetic_operations;
+
+public class p_10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
