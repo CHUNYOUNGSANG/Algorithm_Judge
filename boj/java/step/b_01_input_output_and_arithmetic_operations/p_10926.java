49628aff33dce325ee86b45a6f99d99994d8700e
천영상 <cys07028@naver.com>
2024-08-15 00:40:33 +0900

    [Solve] ??

---
+++ boj/java/step/b_01_input_output_and_arithmetic_operations/p_10926.java
@@ -0,0 +1,14 @@
+package java.step.b_01_input_output_and_arithmetic_operations;
+
+import java.util.Scanner;
+
+public class p_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(str + "??!");
+
+        sc.close();
+    }
+}
