5d8e77ad8169eef9b5785daa84bc0fecce7815aa
천영상 <cys07028@naver.com>
2024-08-15 00:42:21 +0900

    [Solve] 1998년생인 내가 태국에서는 2541년생? !

---
+++ boj/java/step/b_01_input_output_and_arithmetic_operations/p_18108.java
@@ -0,0 +1,14 @@
+package java.step.b_01_input_output_and_arithmetic_operations;
+
+import java.util.Scanner;
+
+public class p_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+
+        System.out.println(y - 543);
+
+        sc.close();
+    }
+}
