f72e8b2a91ba6c2f0929c04587e4dbf9fe79f076
천영상 <cys07028@naver.com>
2024-08-15 00:47:01 +0900

    [Solve] 고양이

---
+++ boj/java/step/b_01_input_output_and_arithmetic_operations/p_10171.java
@@ -0,0 +1,12 @@
+package java.step.b_01_input_output_and_arithmetic_operations;
+
+public class p_10171 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
