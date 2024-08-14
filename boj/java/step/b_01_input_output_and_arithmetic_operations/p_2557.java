a7463167ad37407b20a7037289244a0082f2aab5
천영상 <cys07028@naver.com>
2024-08-15 00:29:49 +0900

    [Solve] Hello World

---
+++ boj/java/step/b_01_input_output_and_arithmetic_operations/p_2557.java
@@ -0,0 +1,8 @@
+package java.step.b_01_input_output_and_arithmetic_operations;
+
+public class p_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
