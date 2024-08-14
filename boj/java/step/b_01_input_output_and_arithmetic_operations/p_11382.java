bc30b4335dfa0ad16f01112c1bc4b90da4576109
천영상 <cys07028@naver.com>
2024-08-15 00:46:06 +0900

    [Solve] 꼬마 정민

---
+++ boj/java/step/b_01_input_output_and_arithmetic_operations/p_11382.java
@@ -0,0 +1,16 @@
+package java.step.b_01_input_output_and_arithmetic_operations;
+
+import java.util.Scanner;
+
+public class p_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+
+        System.out.println(A + B + C);
+
+        sc.close();
+    }
+}
