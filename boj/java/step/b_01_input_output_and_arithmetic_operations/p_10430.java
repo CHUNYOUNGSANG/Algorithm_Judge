7d8a6d8342edfe5eed7ae88b5b024cae9821e1fe
천영상 <cys07028@naver.com>
2024-08-15 00:43:58 +0900

    [Solve] 나머지

---
+++ boj/java/step/b_01_input_output_and_arithmetic_operations/p_10430.java
@@ -0,0 +1,21 @@
+package java.step.b_01_input_output_and_arithmetic_operations;
+
+import java.util.Scanner;
+
+public class p_10430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
+
+        sc.close();
+    }
+
+}
