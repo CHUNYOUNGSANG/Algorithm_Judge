102d79a788cccbf65d0447f052fdf83b328cd8d4
천영상 <chuns7740@gmail.com>
2024-08-30 01:26:50 +0900

    [Solve] 개수 세기

---
+++ boj/java/step/b_04_1_dimensional_array/p_10807.java
@@ -0,0 +1,24 @@
+package java.step.b_04_1_dimensional_array;
+
+import java.util.Scanner;
+
+public class p_10807 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 0;
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int b = sc.nextInt();
+
+        for (int j = 0; j < arr.length; j++) {
+            if(b == arr[j]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
